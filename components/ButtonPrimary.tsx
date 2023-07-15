92564afcce27f3f12c38e188fc70b0e9b3e62a74
Orhan Emre Dikicigil <orhanemre.dev@gmail.com>
2023-07-15 22:29:57 +0300

    style: change button styles

--- components/ButtonPrimary.tsx
+++ components/ButtonPrimary.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from "@/types";
 export default ({ children, onClick, disabled=false, type="button" }: ButtonProps) => {
 
     const classes = `default px-6 py-3 \
-${!disabled ? "bg-emerald-700 cursor-pointer text-white" : "bg-zinc-200 opacity-60"}`;
+${!disabled ? "bg-emerald-800 cursor-pointer text-white" : "bg-zinc-200 opacity-60"}`;
 
     return (
         <button className={classes} onClick={onClick} type={type} disabled={disabled}>
--- components/ButtonSecondary.tsx
+++ components/ButtonSecondary.tsx
@@ -3,7 +3,7 @@ import { ButtonProps } from "@/types";
 export default ({ children, onClick, disabled=false, type="button" }: ButtonProps) => {
 
     const classes = `default px-6 py-3 \
-${!disabled ? "bg-lime-100 cursor-pointer" : "bg-zinc-200 opacity-60"}`;
+${!disabled ? "bg-lime-50 cursor-pointer" : "bg-zinc-200 opacity-60"}`;
 
     return (
         <button className={classes} onClick={onClick} type={type} disabled={disabled}>
