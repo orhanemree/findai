fa34609a12e3eb4fb8bb32ece6315481d94f416f
Orhan Emre Dikicigil <orhanemre.dev@gmail.com>
2023-07-16 00:06:43 +0300

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,18 @@
 import "@/assets/globals.css";
 
+import { Metadata } from "next";
 import { ContextProvider } from "@/context/Context";
 import ToggleLang from "@/components/ToggleLang";
+
+export const metadata: Metadata = {
+    title: "Find AI",
+    description: "Find secret ChatGPT AI in a chat room. Playable game! by Orhan Emre Dikicigil",
+    authors: { name: "Orhan Emre Dikicigil - orhanemree", url: "https://github.com/orhanemree" },
+    openGraph: {
+        title: "Find AI",
+        description: "Find secret ChatGPT AI in a chat room. Playable game! by Orhan Emre Dikicigil"
+    }
+}
 
 export default ({ children }: { children: React.ReactNode }) => {
 
