7ba00cd7d4b4325c2f53d26832f6675d8fe596c2
Orhan Emre Dikicigil <orhanemre.dev@gmail.com>
2023-07-16 21:39:05 +0300

    style: add disabled state

--- components/form/InputText.tsx
+++ components/form/InputText.tsx
@@ -2,9 +2,10 @@ import { InputProps } from "@/types";
 
 interface InputTextProps extends InputProps {
     value?: string,
+    disabled?: boolean
 }
 
-export default ({ onInput, label, value, required=true }: InputTextProps) => {
+export default ({ onInput, label, value, required=true, disabled=false }: InputTextProps) => {
 
     const classes = "default px-3 py-3 outline-none w-full";
 
@@ -14,7 +15,7 @@ export default ({ onInput, label, value, required=true }: InputTextProps) => {
                 <div className="mb-2">
                     {label}
                 </div>}
-            <input className={classes} type="text" value={value} onInput={onInput} required={required} />
+            <input className={classes} type="text" value={value} onInput={onInput} required={required} disabled={disabled}/>
         </div>
     )
 }
